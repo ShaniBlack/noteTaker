d586751e13da3edafda8f116c7c7b92fc8f86f1c
Shani Black <shaniblack2004@gmail.com>
2020-11-29 23:47:30 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const fs = require("fs");
+
+const port = process.env.port || 3000;
+const app = express();
+
+app.use(express.static("public"))
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+
+
+app.listen(PORT, function() {
+console.log("Listening on port: " + PORT!)
+});
+
+app.get("/api/notes", function(req, res) {
+    
+})
